7fef666f39306498f83adafb544eff76afe53d7a
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2018-02-06 16:12:28 +0200

    update most-data component angular 4.x client update (2.0.18)

---
+++ modules/@themost/angular/module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {DataComponent} from './components';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [DataComponent],
+    exports:[DataComponent]
+})
+
+export class MostModule {
+}
