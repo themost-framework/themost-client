bbbe1c87517f63eddbc31a2ca4ff014f0926ec5f
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2019-02-11 23:40:43 +0200

    update @themost/angular client by implementing ng-packagr

--- modules/@themost/angular/packages/client/src/components.ts
+++ modules/@themost/angular/packages/client/src/components.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * MOST Web Framework 2.0 Codename Blueshift
+ * Copyright (c) 2017, THEMOST LP All rights reserved
+ *
+ * Use of this source code is governed by an BSD-3-Clause license that can be
+ * found in the LICENSE file at https://themost.io/license
+ */
 import {Component, Input, OnInit, Output} from '@angular/core';
 import {AngularDataContext} from './client';
 
@@ -8,17 +16,17 @@ import {AngularDataContext} from './client';
 
 export class DataComponent implements OnInit {
 
-    @Input() filter:string;
-    @Input() model:string;
-    @Input() select:string;
-    @Input() group:string;
-    @Input() order:string;
-    @Input() top:number;
-    @Input() count:boolean;
-    @Input() skip:number;
-    @Input() expand:string;
-    @Input() url:string;
-    @Output() value:any;
+    @Input() filter: string;
+    @Input() model: string;
+    @Input() select: string;
+    @Input() group: string;
+    @Input() order: string;
+    @Input() top: number;
+    @Input() count: boolean;
+    @Input() skip: number;
+    @Input() expand: string;
+    @Input() url: string;
+    @Output() value: any;
 
     constructor(protected context: AngularDataContext) {
         //
@@ -32,30 +40,25 @@ export class DataComponent implements OnInit {
         if (typeof this.url === 'string' && this.url.length > 0) {
             q.setUrl(this.url);
         }
-        if (typeof this.filter === 'string' && this.filter.length > 0)
-        {
+        if (typeof this.filter === 'string' && this.filter.length > 0) {
             q.setParam('$filter', this.filter);
         }
-        if (typeof this.select === 'string' && this.select.length > 0)
-        {
+        if (typeof this.select === 'string' && this.select.length > 0) {
             q.setParam('$select', this.select);
         }
-        if (typeof this.group === 'string' && this.group.length > 0)
-        {
+        if (typeof this.group === 'string' && this.group.length > 0) {
             q.setParam('$group', this.group);
         }
-        if (typeof this.order === 'string' && this.order.length > 0)
-        {
+        if (typeof this.order === 'string' && this.order.length > 0) {
             q.setParam('$order', this.order);
         }
-        if (typeof this.expand === 'string' && this.expand.length > 0)
-        {
+        if (typeof this.expand === 'string' && this.expand.length > 0) {
             q.setParam('$expand', this.expand);
         }
-        if (this.skip>0) {
+        if (this.skip > 0) {
             q.setParam('$skip', this.skip);
         }
-        if (this.top>0) {
+        if (this.top > 0) {
             q.setParam('$top', this.top);
         }
         if (this.count) {
@@ -63,26 +66,24 @@ export class DataComponent implements OnInit {
         }
 
         if (this.count) {
-            return q.getList().then((result)=> {
+            return q.getList().then((result) => {
                 this.value = result;
-            }).catch((err)=> {
+            }).catch((err) => {
                 //
             });
         }
-        //set queryable
-        q.getItems().then((result)=> {
+        // set queryable
+        q.getItems().then((result) => {
             if (this.top === 1) {
                 if (result && result.value instanceof Array) {
                     this.value = result.value[0];
-                }
-                else {
+                } else {
                     this.value = result[0];
                 }
-            }
-            else {
+            } else {
                 this.value = result;
             }
-        }).catch((err)=> {
+        }).catch((err) => {
             //
         });
 
--- modules/@themost/angular/packages/client/src/module.ts
+++ modules/@themost/angular/packages/client/src/module.ts
@@ -1,6 +1,14 @@
+/**
+ * @license
+ * MOST Web Framework 2.0 Codename Blueshift
+ * Copyright (c) 2017, THEMOST LP All rights reserved
+ *
+ * Use of this source code is governed by an BSD-3-Clause license that can be
+ * found in the LICENSE file at https://themost.io/license
+ */
 import {NgModule} from '@angular/core';
 import {DataComponent} from './components';
-import {DATA_CONTEXT_CONFIG} from "./client";
+import {DATA_CONTEXT_CONFIG} from './client';
 
 @NgModule({
     imports: [],
@@ -8,14 +16,15 @@ import {DATA_CONTEXT_CONFIG} from "./client";
     exports: [DataComponent],
     providers: [{
         provide: DATA_CONTEXT_CONFIG, useValue: {
-            base: "/",
+            base: '/',
             options: {
                 useMediaTypeExtensions: true
             }
         }
     }]
 })
-
 export class MostModule {
-
+    constructor() {
+        //
+    }
 }
