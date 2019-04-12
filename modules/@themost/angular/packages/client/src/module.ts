46a0858324f596b638e1517afe3048d0d4a5702f
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2019-04-12 15:28:08 +0300

    adds MostModule.forRoot() static method

--- modules/@themost/angular/packages/client/src/module.ts
+++ modules/@themost/angular/packages/client/src/module.ts
@@ -6,25 +6,53 @@
  * Use of this source code is governed by an BSD-3-Clause license that can be
  * found in the LICENSE file at https://themost.io/license
  */
-import {NgModule} from '@angular/core';
+import {ModuleWithProviders, NgModule} from '@angular/core';
 import {DataComponent} from './components';
-import {DATA_CONTEXT_CONFIG} from './client';
+import {AngularDataContext, ClientDataContextConfig, DATA_CONTEXT_CONFIG} from './client';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-    imports: [],
-    declarations: [DataComponent],
-    exports: [DataComponent],
-    providers: [{
-        provide: DATA_CONTEXT_CONFIG, useValue: {
-            base: '/',
-            options: {
-                useMediaTypeExtensions: true
+    imports: [
+        HttpClientModule
+    ],
+    declarations: [
+        DataComponent
+    ],
+    exports: [
+        DataComponent
+    ],
+    providers: [
+        {
+            provide: DATA_CONTEXT_CONFIG, useValue: {
+                base: '/',
+                options: {
+                    useMediaTypeExtensions: true
+                }
             }
-        }
-    }]
+        },
+        AngularDataContext
+    ]
 })
 export class MostModule {
     constructor() {
         //
+    }
+    static forRoot(configuration: ClientDataContextConfig): ModuleWithProviders {
+        // ensure configuration
+        const contextConfiguration = configuration || {
+                base: '/',
+                options: {
+                    useMediaTypeExtensions: true
+                }
+            };
+        return {
+            ngModule: MostModule,
+            providers: [
+                {
+                    provide: DATA_CONTEXT_CONFIG, useValue: contextConfiguration
+                },
+                AngularDataContext
+            ]
+        };
     }
 }
