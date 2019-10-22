097e9202b7945eb2009fe639e53a81997ac5052c
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2019-10-22 18:13:40 +0300

    update karma testing update module version

--- modules/@themost/angular/packages/client/karma.conf.js
+++ modules/@themost/angular/packages/client/karma.conf.js
@@ -10,22 +10,32 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-mocha-reporter'),
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage'),
-      reports: ['html', 'lcovonly'],
+      reports: ['kjhtml', 'mocha'],
       fixWebpackSourcePaths: true
+    },
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--remote-debugging-port=9222'
+        ]
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 8080,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: [ "ChromeHeadlessNoSandbox" ],
     singleRun: false
   });
 };
