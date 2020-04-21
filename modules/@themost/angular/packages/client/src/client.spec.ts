ab5ba94464cd47c93f61765415783d1f2d9f1012
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2020-04-21 20:32:37 +0300

    Update metadata deserialization (#11)
    * add annotations
    
    * update dependencies
    
    * update dependencies

--- modules/@themost/angular/packages/client/src/client.spec.ts
+++ modules/@themost/angular/packages/client/src/client.spec.ts
@@ -40,4 +40,40 @@ describe('AngularDataContext', () => {
             console.log(err);
         });
     }));
+    it('should get Computed annotation', inject([AngularDataContext, HttpClient],
+        async (context: AngularDataContext, http: HttpClient) => {
+        http.post('http://localhost:8081/auth/token', {
+            client_id: '9165351833584149',
+            client_secret: 'hTgqFBUhCfHs/quf/wnoB+UpDSfUusKA',
+            username: 'alexis.rees@example.com',
+            password: 'secret',
+            grant_type: 'password',
+            scope: 'profile'
+        }, {
+            responseType: 'json'
+        }).subscribe(async (res: HttpResponse<any>) => {
+            const token = res.body;
+            // get metadata
+            context.setBearerAuthorization(token.access_token);
+            const metadata = await context.getMetadata();
+            expect(metadata).toBeTruthy();
+            // get entity type
+            const entityType = metadata.EntityType.find( x => {
+                return x.Name === 'User';
+            });
+            expect(entityType).toBeTruthy();
+            const navigationProperty = entityType.NavigationProperty.find( x => {
+                return x.Name === 'createdBy';
+            });
+            expect(navigationProperty).toBeTruthy();
+            expect(navigationProperty.Computed).toBeTruthy();
+            const findAnnotation = navigationProperty.Annotations.find( x => {
+                return x.Term === 'Org.OData.Core.V1.Computed';
+            });
+            expect(findAnnotation).toBeTruthy();
+            expect(findAnnotation.Tag).toBe('true');
+        }, (err) => {
+            console.log(err);
+        });
+    }));
 });
